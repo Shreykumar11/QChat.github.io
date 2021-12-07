442a7d81de5da55b7ad907a6db665a563e959b67
Shrey Kumar Saxena <53331616+Shreykumar11@users.noreply.github.com>
2021-12-08 01:00:50 +0530

    Qchat
    A chat application using django and python.

--- QChat App Django/chat/urls.py
+++ QChat App Django/chat/urls.py
@@ -5,7 +5,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='chat-home'),
+    path('', auth_views.LoginView.as_view(template_name="chat/login.html"), name='chat-login'),
     path('login/', auth_views.LoginView.as_view(template_name="chat/login.html"), name='chat-login'),
     path('logout/', auth_views.LogoutView.as_view(template_name="chat/logout.html"), name='chat-logout'),
     path('register/', views.register, name='chat-register'),
